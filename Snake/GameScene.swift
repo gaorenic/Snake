8501865bb44a9227cc3d425628511d12f7a75aa4
Gabriel Orenic <gaorenic@gmail.com>
2021-05-13 13:35:21 -0500

    The snake moves when swiped and it eats the fruits

--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -15,33 +15,40 @@ struct ColliderType {
     static let redFruit: UInt32 = 1
     static let yellowFruit: UInt32 = 2
     static let snake: UInt32 = 3
+    static let snakeBody: UInt32 = 4
+    static let makeGameBorder: UInt32 = 5
+    
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var snake = SKSpriteNode()
+    var snake = [SKShapeNode]()
+    var snakeBody = SKSpriteNode()
     var redFruit = SKSpriteNode()
     var yellowFruit = SKSpriteNode()
-    var gameBorder = SKSpriteNode()
     var playLabel = SKLabelNode()
+    var direction = "Right"
+    var timer: Timer?
+
+    var snakePB = SKPhysicsBody()
+    var snakeBodyPB = SKPhysicsBody()
     
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor.purple
         self.physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
-        snake.physicsBody?.categoryBitMask = ColliderType.snake
-        snake.physicsBody?.collisionBitMask = ColliderType.redFruit
-        snake.physicsBody?.contactTestBitMask = ColliderType.redFruit
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(moveSnake), userInfo: nil, repeats: true)
+        
+        snakeBody.physicsBody?.categoryBitMask = ColliderType.snakeBody
+        snakeBody.physicsBody?.collisionBitMask = ColliderType.snake
+        snakeBody.physicsBody?.contactTestBitMask = ColliderType.snake
         
         redFruit.physicsBody?.categoryBitMask = ColliderType.redFruit
         
-        snake.physicsBody?.collisionBitMask = ColliderType.yellowFruit
-        snake.physicsBody?.contactTestBitMask = ColliderType.yellowFruit
-        
         yellowFruit.physicsBody?.categoryBitMask = ColliderType.yellowFruit
         
-        makeGameBorder()
+        addSwipeGestureReconizers()
         resetGame()
         
     }
@@ -52,20 +59,104 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeYellowFruit()
     }
     
+    func addSwipeGestureReconizers() {
+        let gestureDirections: [UISwipeGestureRecognizer.Direction] = [.right, .left, .up, .down]
+        for gestureDirection in gestureDirections {
+            let gestureReconzier = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+            gestureReconzier.direction = gestureDirection
+            self.view?.addGestureRecognizer(gestureReconzier)
+        }
+    }
+    
+    @objc func handleSwipe(gesture: UIGestureRecognizer) {
+        if let gesture = gesture as? UISwipeGestureRecognizer {
+            switch gesture.direction {
+            case .up:
+                if direction != "Down" {
+                    direction = "Up"
+                }
+            case .down:
+                if direction != "Up" {
+                    direction = "Down"
+                }
+            case .right:
+                if direction != "Left" {
+                    direction = "Right"
+                }
+            case .left:
+                if direction != "Right" {
+                    direction = "Left"
+                }
+            default:
+                print("No such gesture")
+            }
+        }
+    }
+    
+    @objc func moveSnake() {
+        var headPosition = snake[0].position
+        print("The snake head starting location is \(headPosition)")
+        switch direction {
+        case "Up":
+            headPosition.y += 60
+        case "Down":
+            headPosition.y -= 60
+        case "Right":
+            headPosition.x += 60
+        case "Left":
+            headPosition.x -= 60
+        default:
+            print("move nowhere")
+        }
+//        //snake[0].run(SKAction.move(to: headPosition, duration: 1.0))
+//        for (index, segment) in snake.enumerated() {
+//            if index > 0 {
+//                let segmentPosition = snake[index - 1].position
+//                print("This is segment \(index)")
+//                print("Current location: \(segment.parent!.position)")
+//                print("Moving to       : \(segmentPosition)")
+//                segment.position = segmentPosition
+//                //segment.run(SKAction.move(to: segmentPosition, duration: 1.0))
+//            }
+//        }
+        print("The snake head is moving to \(headPosition)")
+        snake[0].run(SKAction.move(to: headPosition, duration: 1.0))
+    }
+    
     func makeSnake() {
-        snake.removeFromParent()
-        snake = SKSpriteNode(imageNamed: "snake")
-        snake.name = "snake"
-        snake.size = CGSize(width: 60, height: 60)
-        snake.position = CGPoint(x: frame.midX, y: frame.midY)
+        snake = [SKShapeNode]()
+        let head = SKShapeNode(circleOfRadius: 30)
+        head.position = CGPoint(x: frame.midX, y: frame.midY)
+        head.strokeColor = .black
+        head.fillColor = .black
+        head.name = "snake"
         
-        snake.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 60))
-        snake.physicsBody?.isDynamic = true
-        snake.physicsBody?.usesPreciseCollisionDetection = true
-        snake.physicsBody?.friction = 0
-        snake.physicsBody?.affectedByGravity = false
+        head.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 60))
+        head.physicsBody?.isDynamic = true
+        head.physicsBody?.usesPreciseCollisionDetection = true
+        head.physicsBody?.friction = 0
+        head.physicsBody?.affectedByGravity = false
         
-        addChild(snake)
+        head.physicsBody?.categoryBitMask = ColliderType.snake
+        head.physicsBody?.collisionBitMask = ColliderType.redFruit
+        head.physicsBody?.contactTestBitMask = ColliderType.redFruit
+        head.physicsBody?.collisionBitMask = ColliderType.yellowFruit
+        head.physicsBody?.contactTestBitMask = ColliderType.yellowFruit
+        
+        snake.append(head)
+        addChild(head)
+    }
+    
+    func makeSnakeBody() {
+        snakeBody = SKSpriteNode(imageNamed: "snakeBody")
+        snakeBody.name = "snakeBody"
+        snakeBody.size = CGSize(width: 60, height: 60)
+        
+        snakeBody.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 60))
+        snakeBody.physicsBody?.isDynamic = true
+        snakeBody.physicsBody?.usesPreciseCollisionDetection = true
+        snakeBody.physicsBody?.friction = 0
+        snakeBody.physicsBody?.affectedByGravity = false
     }
     
     func makeRedFruit() {
@@ -131,15 +222,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return CGFloat(arc4random_uniform(UInt32(CGFloat(max - min)))) + min
     }
     
-    func makeGameBorder() {
-        gameBorder = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
-        gameBorder.position = CGPoint(x: frame.midX, y: frame.minY + 25)
-        gameBorder.name = "gameBorder"
-        gameBorder.physicsBody = SKPhysicsBody( rectangleOf: gameBorder.size)
-        gameBorder.physicsBody?.isDynamic = false
-        addChild(gameBorder)
-    }
-    
     func makeLabels() {
         playLabel.fontSize = 24
         playLabel.text = ""
@@ -176,15 +258,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             print("yellowFruit contact was made")
             makeYellowFruit()
         }
+        
+        //snakeBody joint
+        if contact.bodyA.node!.name == "snake"
+        {
+            // let joint = SKPhysicsJointFixed.joint(withBodyA: snakePB, bodyB: snakeBodyPB, anchor: CGPoint(x: self.frame - 30, y: self.frame - 30))
+            //            contact.bodyB.node!.removeFromParent()
+        }
+        else
+        {
+            
+        }
+        
+        if firstBody.node?.name == "snake" && secondBody.node?.name == "makeGameBorder" {
+            resetGame()
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
+//        for touch in touches {
+//
+ //           let location = touch.location(in: self)
             
-            let location = touch.location(in: self)
-            
-            snake.position.x = location.x
-            snake.position.y = location.y
-        }
+ //           snake.position.x = location.x
+  //          snake.position.y = location.y
+ //       }
     }
 }
